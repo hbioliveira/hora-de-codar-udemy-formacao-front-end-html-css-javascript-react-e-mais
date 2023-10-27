e510ec570cf9435259f4c14265a3ac50a6ff7acd
Gabriela Oliveira <hbiloliveira@gmail.com>
2023-10-27 16:28:12 -0300

    "mint"

---
+++ 22 - DOM/js/script.js
@@ -0,0 +1,14 @@
+// 1 - Movendo-se pelo DOM
+
+console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+// 2 - 
+
+
+
